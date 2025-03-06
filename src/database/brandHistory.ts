fa491ae6716dd059dec16b2d3e61f6178dc3d029
Vadym <sirenkovadim18@gmail.com>
2025-03-06 18:29:28 +0200

    start

---
+++ src/database/brandHistory.ts
@@ -0,0 +1,12 @@
+export interface IBrand {
+    type: string;
+    id: string;
+    title: string;
+    age: string;
+    price: number;
+    taste: string;
+    country: string;
+    category: string;
+    awards: string;
+}
+
