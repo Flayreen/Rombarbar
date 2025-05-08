40bfe50ca7632b39858761a6169f6d2c8dd79a5d
Oleg Vakarchuk <121119040+Flayreen@users.noreply.github.com>
2025-05-08 03:32:59 +0300

    Merge pull request #5
    * add cocktails list, start working on filter
    
    * add base structure of filter and pagination
    
    * fix bugs with pagination
    
    * pagination minor styling
    
    * add hook useMediaQuery
    
    * add getPageSizeByAdaptive
    
    * start working on drawer filter in mobile and tablet adaptive
    
    * Merge branch 'dev' into cocktails-list
    
    * test
    
    * Merge branch 'dev' into cocktails-list
    
    * mobile and tablet filter, add search params
    
    * add store, characteristic, filter and pagination for brands
    
    * add store, characteristic, filter and pagination for alcohols
    
    * Merge branch 'dev' into cocktails-list
    
    * start working on CocktailRecipe.tsx
    
    * ingredients
    
    * Merge branch 'dev' into cocktails-list

---
+++ src/components/ui/custom-dashed-border.tsx
@@ -0,0 +1,128 @@
+import React, {ReactNode} from 'react';
+
+interface CustomDashedBorderProps {
+    top?: true | TopStyle;
+    right?: true | RightStyle;
+    bottom?: true | BottomStyle;
+    left?: true | LeftStyle;
+    children?: ReactNode;
+}
+
+interface TopStyle {
+    color?: string;
+    stripe?: number;
+    spacing?: number;
+    height?: number | string;
+}
+
+interface RightStyle {
+    color?: string;
+    stripe?: number;
+    spacing?: number;
+    height?: undefined;
+    width?: number | string;
+}
+
+interface BottomStyle {
+    color?: string;
+    stripe?: number;
+    spacing?: number;
+    height?: number | string;
+}
+
+interface LeftStyle {
+    color?: string;
+    stripe?: number;
+    spacing?: number;
+    width?: number | string;
+}
+
+const buildStyle = (
+    position: 'top' | 'right' | 'bottom' | 'left',
+    style: true | TopStyle | RightStyle | BottomStyle | LeftStyle | undefined
+): React.CSSProperties => {
+    if (style === undefined) {
+        return {};
+    }
+
+    const defaultStyle = {
+        color: '#000',
+        stripe: 10,
+        spacing: 10,
+        height: ['top', 'bottom'].includes(position) ? '1px' : '100%',
+        width: ['right', 'left'].includes(position) ? '1px' : '100%',
+    };
+
+    const customStyle = {
+        ...defaultStyle,
+        ...(style === true ? {} : Object.fromEntries(Object.entries(style).map(([key, value]) => {
+            if (['height', 'width'].includes(key)) {
+                return [key, `${value}px`];
+            }
+            return [key, value];
+        }))),
+    };
+
+    const direction = ['top', 'bottom'].includes(position) ? 'to right' : 'to bottom';
+    const {color, stripe, spacing, height, width} = customStyle;
+    const size = stripe + spacing;
+    const ratio = 100 * stripe / size;
+
+    return {
+        position: 'absolute',
+        [position]: 0,
+        backgroundImage: `linear-gradient(${direction}, ${color}, ${color} ${ratio}%, transparent ${ratio}%, transparent 100%)`,
+        backgroundSize: `${size}px ${size}px`,
+        height,
+        width,
+    };
+};
+
+export const CustomDashedBorder: React.FunctionComponent<CustomDashedBorderProps> = ({
+     top,
+     right,
+     bottom,
+     left,
+     children,
+}) => {
+    return (
+        <>
+            {top && <div style={buildStyle('top', top)}></div>}
+            {right && <div style={buildStyle('right', right)}></div>}
+            {bottom && <div style={buildStyle('bottom', bottom)}></div>}
+            {left && <div style={buildStyle('left', left)}></div>}
+            {children}
+        </>
+    );
+};
+
+interface CustomDashedBorderBase extends CustomDashedBorderProps {
+    className?: string;
+    withoutBorders?: boolean;
+}
+
+export default function CustomDashedBorderBase({
+    className,
+    top,
+    right,
+    bottom,
+    left,
+    withoutBorders = false,
+    children
+} : CustomDashedBorderBase) {
+
+    return (
+        <div className="relative">
+            <CustomDashedBorder
+                top={withoutBorders ? undefined : top || {color: "#512E1240", height: 1, stripe: 10, spacing: 10}}
+                right={withoutBorders ? undefined : right || {color: "#512E1240", width: 1, stripe: 10, spacing: 10}}
+                bottom={withoutBorders ? undefined : bottom || {color: "#512E1240", height: 1, stripe: 10, spacing: 10}}
+                left={withoutBorders ? undefined : left || {color: "#512E1240", width: 1, stripe: 10, spacing: 10}}
+            >
+                <div className={className}>
+                    {children}
+                </div>
+            </CustomDashedBorder>
+        </div>
+    )
+}
--- src/pages/CocktailRecipe.tsx
+++ src/pages/CocktailRecipe.tsx
@@ -1,7 +1,49 @@
+import cocktailsList from "../database/cocktails.json"
+import {ICocktail} from "@/types/cocktail/ICocktail.ts";
+import {useNavigate, useParams} from "react-router-dom";
+import {NotFound} from "@/pages/NotFound.tsx";
+import ArrowLeftIcon from "@/components/ui/arrows/ArrowLeftIcon.tsx";
+import CustomDashedBorderBase from "@/components/ui/custom-dashed-border.tsx";
+import {useMediaQuery} from "@/hooks/use-media-query.tsx";
+
 export const CocktailRecipe = () => {
+    const { id } = useParams();
+    const navigate = useNavigate();
+    const isMobile: boolean = useMediaQuery("(max-width: 640px)");
+    const currentCocktail: ICocktail | undefined = cocktailsList.find((cocktail: ICocktail) => cocktail.id === id);
+
+    if (!currentCocktail) {
+        return <NotFound/>
+    }
+
     return (
-        <div>
-            recipe
+        <div className="container">
+            <div className="flex justify-between mt-6 md:mt-8 items-center">
+                <button onClick={() => navigate(-1)} className="text-black transition-colors">
+                    <ArrowLeftIcon className="w-7 h-7"/>
+                </button>
+                <div className="flex flex-col items-center gap-2">
+                    <h2 className="font-display-georgia uppercase text-[16px] md:text-[24px] xl:text-[32px] font-bold tracking-[0.1em] text-primary">
+                        {currentCocktail.title}
+                    </h2>
+                    <div className="w-28 h-0.25 bg-primary"></div>
+                </div>
+                <div className="w-7 h-7"></div>
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-[72px] mt-6 md:mt-[56px] lg:mt-[64px]">
+                <CustomDashedBorderBase className="md:p-4 lg:p-8" withoutBorders={isMobile}>
+                    <img
+                        className="aspect-[1.1/1] w-full object-cover rounded-[6px]"
+                        src={currentCocktail.imageUrl}
+                        alt={currentCocktail.title}
+                    />
+                </CustomDashedBorderBase>
+
+                <div className="flex flex-col items-center">
+                    <span>ІНГРЕДІЄНТИ:</span>
+                </div>
+            </div>
         </div>
     );
 };
